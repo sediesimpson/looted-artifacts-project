45bfabc3675fa93bd4463f53397951cc84c660a9
Sedinam Simpson <sms227@login-p-1.data.cluster>
2024-05-24 12:18:04 +0100

    moved similarity

--- similarity.py
+++ similarity.py
@@ -3,14 +3,23 @@ import torch.nn as nn
 from torchvision import models, transforms
 from PIL import Image
 import numpy as np
-from finaldataloader import CustomImageDataset
+#from finaldataloader import CustomImageDataset
 from tqdm import tqdm
 from torch.utils.data import Dataset, DataLoader, SubsetRandomSampler
 import os
+import sys
 from scipy.spatial.distance import cosine
 import matplotlib.pyplot as plt
 import cv2
 from sklearn.neighbors import KNeighborsClassifier
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader, SubsetRandomSampler
+import os
+from PIL import Image
+import torch
+import numpy as np
+import cv2
+
 #------------------------------------------------------------------------------------------------------------------------
 # Define the CustomClassifier module
 #------------------------------------------------------------------------------------------------------------------------
@@ -127,7 +136,7 @@ preprocess = transforms.Compose([
 # Load dataset
 #------------------------------------------------------------------------------------------------------------------------
 
-dataset_dir = "/Users/sedisimpson/Desktop/Dissertation Data/Test Dataset 5"
+dataset_dir = "/rds/user/sms227/hpc-work/dissertation/data/Test Dataset 4"
 dataset = CustomImageDataset(root_dir=dataset_dir)
 
 #------------------------------------------------------------------------------------------------------------------------
@@ -139,7 +148,7 @@ dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
 # Model
 #------------------------------------------------------------------------------------------------------------------------
 num_classes = 3
-hidden_features = 2
+hidden_features = 64
 model = CustomResNet50(num_classes=num_classes, hidden_features=hidden_features)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -155,49 +164,49 @@ print("Shape of labels:", labels.shape)
 #------------------------------------------------------------------------------------------------------------------------
 # Query, similarity and top N images using cosine similarity
 #------------------------------------------------------------------------------------------------------------------------
-query_image_path = "/Users/sedisimpson/Desktop/Dissertation Data/Test Dataset 5/Accessories/Barakat Volume-11, FZ210.JPG"
+query_image_path = "/rds/user/sms227/hpc-work/dissertation/data/Test Dataset 4/Accessories/Barakat Volume-11, FZ210.JPG"
 query_features, query_label = extract_query_features(query_image_path, dataset, model, device)
-# similarities = compute_similarities(query_features, features)
-# n = 4
-# top_n_indices = retrieve_top_n_similar(similarities, img_paths, query_image_path, n=n)
-# print("Top N similar image indices:", top_n_indices)
-# print("Top N similar image indices:", top_n_indices)
+similarities = compute_similarities(query_features, features)
+n = 4
+top_n_indices = retrieve_top_n_similar(similarities, img_paths, query_image_path, n=n)
+print("Top N similar image indices:", top_n_indices)
+print("Top N similar image indices:", top_n_indices)
 
 #------------------------------------------------------------------------------------------------------------------------
 # Query, similarity and top N images using KNN classifier
 #------------------------------------------------------------------------------------------------------------------------
-from sklearn.metrics import roc_curve, auc
+# from sklearn.metrics import roc_curve, auc
 
-# Compute distances for a validation set
-validation_distances = []
-validation_labels = []
-for val_img_features, val_label in zip(validation_distances, validation_labels):
-    distances, _ = knn.kneighbors([val_img_features], n_neighbors=1, return_distance=True)
-    validation_distances.append(distances[0][0])
-    validation_labels.append(val_label)
+# # Compute distances for a validation set
+# validation_distances = []
+# validation_labels = []
+# for val_img_features, val_label in zip(validation_distances, validation_labels):
+#     distances, _ = knn.kneighbors([val_img_features], n_neighbors=1, return_distance=True)
+#     validation_distances.append(distances[0][0])
+#     validation_labels.append(val_label)
 
 
-# Define true labels and predicted scores based on distances
-true_labels = [1 if val_label == query_label else 0 for val_label in validation_labels]
-predicted_scores = [-dist for dist in validation_distances]  # Use negative distance as score
+# # Define true labels and predicted scores based on distances
+# true_labels = [1 if val_label == query_label else 0 for val_label in validation_labels]
+# predicted_scores = [-dist for dist in validation_distances]  # Use negative distance as score
 
-# Compute ROC curve and AUC
-fpr, tpr, thresholds = roc_curve(true_labels, predicted_scores)
-roc_auc = auc(fpr, tpr)
+# # Compute ROC curve and AUC
+# fpr, tpr, thresholds = roc_curve(true_labels, predicted_scores)
+# roc_auc = auc(fpr, tpr)
 
-plt.figure()
-plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (area = {roc_auc:.2f})')
-plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver Operating Characteristic')
-plt.legend(loc="lower right")
-plt.show()
+# plt.figure()
+# plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (area = {roc_auc:.2f})')
+# plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+# plt.xlabel('False Positive Rate')
+# plt.ylabel('True Positive Rate')
+# plt.title('Receiver Operating Characteristic')
+# plt.legend(loc="lower right")
+# plt.show()
 
-# Choose the threshold with the best balance (e.g., max TPR - FPR)
-optimal_idx = np.argmax(tpr - fpr)
-optimal_threshold = thresholds[optimal_idx]
-print(f'Optimal Threshold: {optimal_threshold}')
+# # Choose the threshold with the best balance (e.g., max TPR - FPR)
+# optimal_idx = np.argmax(tpr - fpr)
+# optimal_threshold = thresholds[optimal_idx]
+# print(f'Optimal Threshold: {optimal_threshold}')
 
 # # Define the KNN classifier
 # knn = KNeighborsClassifier(n_neighbors=3, metric='cosine') # here we use cosine distance to make it comparable, but can also be euclidean distance
@@ -233,25 +242,25 @@ print(f'Optimal Threshold: {optimal_threshold}')
 #------------------------------------------------------------------------------------------------------------------------
 
 # Retrieve file paths and labels of the top N similar images
-# top_n_image_paths = [img_paths[i] for i in top_n_indices]
-# top_n_labels = [labels[i] for i in top_n_indices]
-# print(top_n_image_paths)
+top_n_image_paths = [img_paths[i] for i in top_n_indices]
+top_n_labels = [labels[i] for i in top_n_indices]
+print(top_n_image_paths)
 
 
-# def visualize_similar_images(query_image_path, top_n_image_paths, top_n_labels, n=n):
-#     plt.figure(figsize=(15, 5))
-#     # Plot the query image
-#     query_img = Image.open(query_image_path).convert('RGB')
-#     plt.subplot(1, n+1, 1)
-#     plt.imshow(query_img)
-#     plt.title(f"Query Image (Label: {query_label})")
-#     plt.axis('off')
-#     # Plot the top N similar images
-#     for i, (img_path, label) in enumerate(zip(top_n_image_paths, top_n_labels), 1):
-#         img = Image.open(img_path).convert('RGB')
-#         plt.subplot(1, n+1, i+1)
-#         plt.imshow(img)
-#         plt.title(f"Label: {label}")
-#         plt.axis('off')
-#     plt.show()
-# visualize_similar_images(query_image_path, top_n_image_paths, top_n_labels, n=n)
+def visualize_similar_images(query_image_path, top_n_image_paths, top_n_labels, n=n):
+    plt.figure(figsize=(15, 5))
+    # Plot the query image
+    query_img = Image.open(query_image_path).convert('RGB')
+    plt.subplot(1, n+1, 1)
+    plt.imshow(query_img)
+    plt.title(f"Query Image (Label: {query_label})")
+    plt.axis('off')
+    # Plot the top N similar images
+    for i, (img_path, label) in enumerate(zip(top_n_image_paths, top_n_labels), 1):
+        img = Image.open(img_path).convert('RGB')
+        plt.subplot(1, n+1, i+1)
+        plt.imshow(img)
+        plt.title(f"Label: {label}")
+        plt.axis('off')
+    plt.savefig('plots/top images')
+visualize_similar_images(query_image_path, top_n_image_paths, top_n_labels, n=n)
