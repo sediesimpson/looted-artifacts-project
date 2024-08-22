2fc417969aa711c44800277b8c5ea06afdbd036c
Sedinam Simpson <sms227@login-p-3.data.cluster>
2024-08-22 21:51:18 +0100

    radius check

--- knn2nn.py
+++ knn2nn.py
@@ -38,19 +38,14 @@ from sklearn.utils import shuffle
 #------------------------------------------------------------------------------------------------------------------------
 # Define CustomResNetClassifier
 #------------------------------------------------------------------------------------------------------------------------
-class CustomResNetClassifier(nn.Module):
-    def __init__(self, hidden_dim, num_classes, weights=None):
-        super(CustomResNetClassifier, self).__init__()
+class CustomResNetExtractor(nn.Module):
+    def __init__(self, weights=None):
+        super(CustomResNetExtractor, self).__init__()
         self.resnet = models.resnet50(weights=weights)
         self.resnet.requires_grad_(False)  # Freeze all layers
 
         # Replace the fully connected layer with a custom sequential layer
-        self.resnet.fc = nn.Sequential(
-            nn.Linear(2048, hidden_dim),
-            nn.ReLU(),
-            nn.Dropout(),
-            nn.Linear(hidden_dim, num_classes)  # Output layer with one neuron per class for BCEWithLogitsLoss
-        )
+        self.resnet.fc = torch.nn.Identity()
 
     def forward(self, x):
         x = self.resnet(x)
@@ -68,10 +63,10 @@ torch.cuda.manual_seed_all(42)
 # Create train dataset
 root_dir = "/rds/user/sms227/hpc-work/dissertation/data/duplicatedata"
 train_dataset = CustomImageDatasetTrain(root_dir)
-test_dataset = CustomImageDatasetTest(root_dir)
+#test_dataset = CustomImageDatasetTest(root_dir)
 
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+#test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
 #------------------------------------------------------------------------------------------------------------------------
 # Define the feature extraction function
@@ -81,59 +76,48 @@ def extract_features(dataloader, model, device):
     features_list = []
     labels_list = []
     img_paths_list = []
+    label_counts_list = []
     
     with torch.no_grad():
-        for imgs, labels, img_paths in tqdm(dataloader, desc="Extracting features"):
+        for imgs, labels, img_paths, label_counts in tqdm(dataloader, desc="Extracting features"):
             # Move images to the specified device
             imgs = imgs.to(device)
             
             # Extract features
-            features = model.resnet.conv1(imgs)
-            features = model.resnet.bn1(features)
-            features = model.resnet.relu(features)
-            features = model.resnet.maxpool(features)
-            features = model.resnet.layer1(features)
-            features = model.resnet.layer2(features)
-            features = model.resnet.layer3(features)
-            features = model.resnet.layer4(features)
-            features = model.resnet.avgpool(features)
-            features = torch.flatten(features, 1)
+            features = model(imgs)
+            #print('feature dimensions:',features.shape)
             
             # Append features and labels to the respective lists
             features_list.append(features.cpu().numpy())
             labels_list.append(labels.cpu().numpy())
             img_paths_list.extend(img_paths)
+            label_counts_list.extend(label_counts)
+ 
 
     # Flatten the list of arrays (handle varying batch sizes)
     features_array = np.concatenate(features_list, axis=0)
     labels_array = np.concatenate(labels_list, axis=0)
+
     
-    return features_array, labels_array, img_paths_list
+    return features_array, labels_array, img_paths_list, label_counts_list
 
 #------------------------------------------------------------------------------------------------------------------------
 # Model
 #------------------------------------------------------------------------------------------------------------------------
-num_classes = train_dataset.count_unique_labels()
-hidden_dim = 256  # Intermediate hidden layer size
+# num_classes = train_dataset.count_unique_labels()
+# hidden_dim = 256  # Intermediate hidden layer size
+# weights = models.ResNet50_Weights.DEFAULT  # Pre-trained weights
+# model = CustomResNetClassifier(hidden_dim, num_classes, weights=weights)
 weights = models.ResNet50_Weights.DEFAULT  # Pre-trained weights
-model = CustomResNetClassifier(hidden_dim, num_classes, weights=weights)
-
+model = CustomResNetExtractor(weights=weights)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = model.to(device)
 
 # Extract features for training and validation sets
-train_features, train_labels, train_img_paths = extract_features(train_loader, model, device)
+train_features, train_labels, train_img_paths, train_label_counts = extract_features(train_loader, model, device)
 
-
-num_classes = test_dataset.count_unique_labels()
-hidden_dim = 256  # Intermediate hidden layer size
-weights = models.ResNet50_Weights.DEFAULT  # Pre-trained weights
-model = CustomResNetClassifier(hidden_dim, num_classes, weights=weights)
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = model.to(device)
 # Extract features for test set 
-test_features, test_labels, test_img_paths = extract_features(test_loader, model, device)
+# test_features, test_labels, test_img_paths = extract_features(test_loader, model, device)
 
 #------------------------------------------------------------------------------------------------------------------------
 # Find the optimal distance threshold for the knn method, using precision
@@ -178,7 +162,78 @@ nbrs = NearestNeighbors(metric='cosine', algorithm='brute', radius=1.0).fit(trai
 # for i in filtered_indices:
 #     print(train_img_paths[i])
 
+#------------------------------------------------------------------------------------------------------------------------
+# Find the optimal radius
+#------------------------------------------------------------------------------------------------------------------------
+radii = np.linspace(0.05, 0.5, 100)
 
+best_precision = 0
+best_radius = 0 
+best_recall = 0
+
+recalls = []
+
+for radius in radii:
+    true_positives = 0
+    false_positives = 0
+    true_negatives = 0
+    false_negatives = 0 
+    
+    nbrs = NearestNeighbors(metric='cosine', algorithm='brute', radius=radius).fit(train_features)
+
+    for query_index in range(len(train_features)):
+        distances, indices = nbrs.radius_neighbors([train_features[query_index]], sort_results=True)
+
+        distances = distances[0][1:] # remove first distance as it is itself
+        indices = indices[0][1:] # remove first index as it is itself
+
+        true_label = train_labels[query_index]
+        neighbor_labels = train_labels[indices]
+
+        if neighbor_labels.size > 0:
+            for label in neighbor_labels:
+                if label == true_label:
+                    true_positives +=1 
+                else:
+                    true_negatives +=1 
+
+            fn = train_label_counts[query_index] - true_positives
+
+            if fn < 0:
+                fn = 0
+
+            false_negatives += fn
+
+    if (true_positives + false_negatives) > 0:
+        recall = true_positives / (true_positives + false_negatives)
+    else:
+        recall = 0
+
+    recalls.append(recall)
+    if recall >= best_recall:
+        best_recall = recall
+        best_radius = radius
+
+        
+print(f"Best radius: {best_radius} with recall: {best_recall}")
+
+
+        
+
+
+
+
+
+sys.exit()
+#------------------------------------------------------------------------------------------------------------------------
+# Find the optimal distance threshold for the knn method, using precision
+#------------------------------------------------------------------------------------------------------------------------
+
+# Range of thresholds to evaluate
+thresholds = np.linspace(0.01, 0.1, 100)
+
+# We set a large radius to capture all potential NNs
+nbrs = NearestNeighbors(metric='cosine', algorithm='brute', radius=1.0).fit(train_features)
 
 best_precision = 0
 best_threshold = 0 
@@ -194,11 +249,12 @@ for threshold in thresholds:
         
         for query_index in range(len(train_features)):
             distances, indices = nbrs.radius_neighbors([train_features[query_index]], sort_results=True)
+            print('distances:', distances)
 
-            distances = [dist[1:] for dist in distances] # remove first distance as it is itself
-            indices = [idx[1:] for idx in indices] # remove first index as it is itself
+            distances = distances[0][1:] # remove first distance as it is itself
+            indices = indices[0][1:] # remove first index as it is itself
 
-            filtered_indices = [index for dist, index in zip(distances[0], indices[0]) if dist < threshold]
+            filtered_indices = [index for dist, index in zip(distances, indices) if dist < threshold]
 
             if filtered_indices:
                 # For simplicity, use the most common label among neighbors as the prediction
@@ -242,31 +298,6 @@ for threshold in thresholds:
 
 print(f"Best threshold: {best_threshold} with precision: {best_precision}")
 
-sys.exit()
 
-#np.save('precisions/precision3.npy', precision)
-
-
-
-# # Convert list to numpy array for easier manipulation
-# precisions_over_runs = np.array(precisions_over_runs)
-
-# # Calculate mean and standard deviation
-# mean_precisions = np.mean(precisions_over_runs, axis=0)
-# std_precisions = np.std(precisions_over_runs, axis=0)
-
-# # Plot mean precision with confidence bands
-# plt.figure(figsize=(10, 6))
-# plt.plot(thresholds, mean_precisions, label='Mean Precision')
-# plt.fill_between(thresholds, 
-#                 mean_precisions - std_precisions, 
-#                 mean_precisions + std_precisions, 
-#                 color='b', alpha=0.2, label='Confidence Band (1 Standard Deviation)')
-# plt.xlabel('Distance Threshold')
-# plt.ylabel('Precision')
-# # plt.title('Precision vs. DistanceThreshold with Confidence Bands')
-# plt.legend()
-# plt.grid(True)
-# plt.savefig('plots/distancevprecision3.png')
         
 
