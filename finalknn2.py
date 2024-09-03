e104d9c42224b5e49aaae717688af928fcd1cf87
Sedinam Simpson <sms227@gpu-q-3.data.cluster>
2024-09-03 20:23:54 +0100

    confusion matrix added

--- finalknn2.py
+++ finalknn2.py
@@ -84,6 +84,8 @@ root_dir = "/rds/user/sms227/hpc-work/dissertation/data/duplicatedata"
 train_dataset = CustomImageDatasetTrain(root_dir)
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
 
+test_dataset = CustomImageDatasetTest(root_dir)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 #--------------------------------------------------------------------------------------------------------------------------
 # Load checkpoints from trained model with updated weights
 #--------------------------------------------------------------------------------------------------------------------------
@@ -131,6 +133,71 @@ def extract_features(dataloader, model, device):
 # Extract features for training and validation sets
 train_features, train_labels, train_img_paths, train_label_counts = extract_features(train_loader, model, device)
 
+test_features, test_labels, test_img_paths, test_label_counts = extract_features(test_loader, model, device)
+
+#------------------------------------------------------------------------------------------------------------------------
+# Running the knn
+#------------------------------------------------------------------------------------------------------------------------
+print(len(train_features))
+print('length of train labels:',len(train_labels))
+
+query_index = 88
+nbrs = NearestNeighbors(metric='cosine', algorithm='brute', radius=0.004).fit(train_features)
+distances, indices = nbrs.radius_neighbors([train_features[query_index]], sort_results=True)
+print(len(distances[0]))
+
+print('===========  INDICES ============')
+print(indices)
+print()
+print('=========== DISTANCES ============')
+print(distances)
+print()
+print('=========== TEST IMAGE PATH ============')
+imgs_to_plot = []
+print(train_img_paths[query_index])
+#imgs_to_plot.append(train_img_paths[query_index])
+print()
+print('=========== NEIGHBOUR IMAGE PATHS ============')
+for i in indices[0]:
+    imgs_to_plot.append(train_img_paths[i])
+print(imgs_to_plot)
+
+print('=========== PLOTTING IMAGES ============')
+# Number of images
+num_images = len(imgs_to_plot)
+
+# Maximum number of columns
+max_cols = 5
+rows = math.ceil(num_images / max_cols)  # Calculate the number of rows needed
+
+# Create a figure with a dynamic grid of subplots (multiple rows if needed)
+fig, axes = plt.subplots(rows, min(num_images, max_cols), figsize=(min(num_images, max_cols) * 5, rows * 5))  # Adjust figsize as needed
+
+# Flatten axes array for easier iteration if grid has more than 1 row
+axes = axes.flatten() if num_images > 1 else [axes]
+
+# Loop through each image path and plot them
+for i, img_path in enumerate(imgs_to_plot):
+    img = Image.open(img_path)  # Open the image
+    axes[i].imshow(img)  # Display the image
+    axes[i].axis('off')  # Hide the axes
+
+    # Add labels
+    if i == 0:
+        axes[i].set_title("Query Image")
+    else:
+        axes[i].set_title(f"Neighbour {i}")
+
+# Hide any unused subplots
+for j in range(i + 1, len(axes)):
+    axes[j].axis('off')
+
+# Show the plot with all images
+plt.savefig('Aplotsknn/query88FT.pdf')
+
+
+sys.exit()
+
 #------------------------------------------------------------------------------------------------------------------------
 # Find the optimal radius
 #------------------------------------------------------------------------------------------------------------------------
@@ -173,8 +240,12 @@ print('Distribution:', len(distribution))
 
 # Overlay a KDE line
 
+
 fig, ax = plt.subplots()
-dist_plot = sns.kdeplot(distribution, color='r', linewidth=2)
+dist_plot = sns.kdeplot(distribution, color='teal')
+
+ax.set_xlabel('Radius')
+ax.set_ylabel('Density')
 
 lines_for_plot = dist_plot.get_lines()
 print('lines for plot:',lines_for_plot)
@@ -185,8 +256,120 @@ for line in lines_for_plot:
     print(x[np.argmax(y)])
     ax.axvline(x[np.argmax(y)], ls='--', color='black')
 
-
-plt.savefig('Aplotsknn/distribution2.png')
-
+plt.savefig('Aplotsknn/distributionft.pdf')
 
 
+#----------------------------------------------------------------------------------------------------------------------------
+# Figuring out query time
+#----------------------------------------------------------------------------------------------------------------------------
+save_dir = 'Adatasetsizes/'
+# Initialize lists to store dataset sizes and query times
+dataset_sizes = []
+query_times = []
+
+subset_sizes1 = range(10,100,10)
+subset_sizes2 = range(100,1000,100)
+subset_sizes3 = range(1000,3500,500)
+subset_sizes = np.concatenate((subset_sizes1, subset_sizes2, subset_sizes3))
+
+# Number of queries to average for each subset size
+num_queries = 10
+
+nbrs = NearestNeighbors(metric='cosine', algorithm='brute', radius=0.004).fit(train_features)
+
+# Iterate through each subset size
+for subset_size in subset_sizes:
+    # Subset the test dataset
+    subset_indices = np.random.choice(len(train_features), subset_size, replace=False)
+    subset_train_features = train_features[subset_indices]
+
+
+      # Measure the query times for multiple queries
+    total_query_time = 0
+    for _ in range(num_queries):
+
+        start_time = time.time()
+
+        for query_index in range(len(subset_train_features)):
+            # Find all the neighbors
+            distances, indices = nbrs.radius_neighbors([subset_train_features[query_index]], sort_results=True)
+
+        # End time for the query
+        end_time = time.time()
+        query_time = end_time - start_time
+
+        # Accumulate the query time
+        total_query_time += query_time
+
+    # Calculate the average query time for this subset size
+    average_query_time = total_query_time / num_queries
+    dataset_sizes.append(subset_size)
+    query_times.append(average_query_time)
+
+    print(f"Subset Size: {subset_size}, Average Query Time: {average_query_time}")
+
+# Optionally, you can save the dataset_sizes and query_times for further analysis
+np.save(os.path.join(save_dir, "dataset_sizes_KNN_FT3.npy"), np.array(dataset_sizes))
+np.save(os.path.join(save_dir, "query_times_KNN_FT3.npy"), np.array(query_times))
+
+
+#----------------------------------------------------------------------------------------------------------------------------
+# Testing the precision and recall 
+#----------------------------------------------------------------------------------------------------------------------------
+true_positives = 0
+false_positives = 0
+true_negatives = 0
+false_negatives = 0 
+precision = 0
+recall = 0
+
+nbrs = NearestNeighbors(metric='cosine', algorithm='brute', radius=0.004).fit(test_features)
+
+for query_index in range(len(test_features)):
+    distances, indices = nbrs.radius_neighbors([test_features[query_index]], sort_results=True)
+
+    distances = distances[0][1:] # remove first distance as it is itself
+    indices = indices[0][1:] # remove first index as it is itself
+
+    non = len(indices) # how many neighbors are returned 
+
+    true_label = test_labels[query_index]
+    neighbor_labels = test_labels[indices]
+
+    if neighbor_labels.size > 0:
+        for label in neighbor_labels:
+            if label == true_label:
+                true_positives +=1 
+
+        fn = test_label_counts[query_index] - true_positives
+
+        if fn < 0:
+            fn = 0
+
+        false_negatives += fn
+
+        tn = len(test_features) - false_negatives
+
+        if tn < 0: 
+            tn = 0 
+
+        true_negatives += tn
+
+        fp = len(neighbor_labels) - true_positives
+
+        if fp < 0:
+            fp = 0
+
+        false_positives += fp
+
+if (true_positives + false_negatives) > 0:
+    recall = true_positives / (true_positives + false_negatives)
+else:
+    recall = 0
+
+if (true_positives + false_positives) > 0:
+    precision = true_positives / (true_positives + false_positives) 
+else:
+    precision = 0
+
+print(f'True positives:{true_positives}, false positives:{false_positives}, true negatives: {true_negatives}, false negatives = {false_negatives}, recall: {recall}, precision:{precision}')
